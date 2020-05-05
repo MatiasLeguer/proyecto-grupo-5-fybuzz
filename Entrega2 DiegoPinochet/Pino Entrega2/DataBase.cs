4655cfa332a13d9d5852fd1ab4e489c358e32022
Diego Pinochet <dppinochet@miuandes.cl>
2020-05-05 15:09:45 -0400

    Clases DiegoPinochet

---
+++ Entrega2 DiegoPinochet/Pino Entrega2/DataBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pino_Entrega2
+{
+    class DataBase: IOperationMult
+    {
+        protected List<Song> listSongsGlobal;
+        protected List<Video> listVideoGlobal; //get y un set
+        protected List<Playlist> listPlaylistGlobal;
+        protected List<String> gender;
+
+        private Dictionary<int, List<string>> userDataBase;
+    }
+}
---
+++ Entrega2 DiegoPinochet/Pino Entrega2/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pino_Entrega2
+{
+    class Player
+    {
+        //Hay que usar eventos que vengan desde menú y triguereen los métodos de aca con las canciones de Database.
+        public void Play()
+        {
+            Consol
+        }
+        public void Stop()
+        {
+
+        }
+        public void Skip()
+        {
+
+        }
+        public void Previous()
+        {
+
+        }
+        public void Random()
+        {
+
+        }
+    }
+}
