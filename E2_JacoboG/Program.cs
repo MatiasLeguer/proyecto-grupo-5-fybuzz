ceba092198f3c9845ca4c205ebff31f1e1b91338
Juan Jacobo Giraldo Rodriguez <juanjacobogiraldorodriguez@Juans-MacBook-Air-2.local>
2020-05-08 20:17:23 -0400

    Implementacion clases

--- E2_JacoboG/Program.cs
+++ E2_JacoboG/Program.cs
@@ -8,6 +8,7 @@ namespace E2_JacoboG
         public static void Main(string[] args)
         {
             Console.WriteLine("Program Vacio hasta Implementacion Menu");
+            Console.WriteLine("Prueba");
         }
     }
 }
