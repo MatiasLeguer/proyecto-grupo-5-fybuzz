5e471fbc2fadcdb7f904796fabdf5fe48d04c995
MatiasLeguer <62114519+MatiasLeguer@users.noreply.github.com>
2020-05-23 21:12:00 -0400

    Clase User ordenada, Clase Menu ordenada hasta la linea 323

--- FyBuzz_E2/User.cs
+++ FyBuzz_E2/User.cs
@@ -9,6 +9,7 @@ namespace FyBuzz_E2
     [Serializable]
     public class User
     {
+        //ATRIBUTOS:
 
         protected int registerNumber;
         protected string username;
@@ -22,9 +23,13 @@ namespace FyBuzz_E2
         protected bool adsOn;
         protected bool privacy;
         protected List<PlayList> profilePlaylists = new List<PlayList>();
+        protected List<Profile> perfiles = new List<Profile>();
+        //--------------------------------------------------------------------------------------------------
 
-        protected List<Profile> perfiles = new List<Profile>(); 
 
+
+        //GETTERS Y SETTERS
+        //--------------------------------------------------------------------------------------------------
         public string Username { get => username; set => username = value; }
         public string Password { get => password; set => password = value; }
         public string Email { get => email; set => email = value; }
@@ -37,35 +42,47 @@ namespace FyBuzz_E2
         public List<Profile> Perfiles { get => perfiles; set => perfiles = value; }
         public List<string> FollowingList { get => followingList; set => followingList = value; }
         public List<PlayList> ProfilePlaylists { get => profilePlaylists; set => profilePlaylists = value; }
+        //--------------------------------------------------------------------------------------------------
 
+
+        //METODOS:
+
+        //MÉTODO PARA CREAR UN PERFIL
+        //--------------------------------------------------------------------------------------------------
         public void CreateProfile(string pname, string ppic, string ptype, string pmail, string pgender, int page)
         {
             Profile profileX = new Profile(pname, ppic, ptype, pmail, pgender, page);
             perfiles.Add(profileX);
         }
+        //--------------------------------------------------------------------------------------------------
 
-        public List<string> AccountSettings()
+
+
+        //MÉTODOS PARA ENTREGAR INFORMACIÓN DE LA CLASE
+        //--------------------------------------------------------------------------------------------------
+        public List<string> AccountSettings()                      //Entrega la lista de informacion del usuario seleccionado
         {
-            // Metodo que entrega la lista de informacion del usuario seleccionado
-            List<string> Settings = new List<string>() { username, password, email, accountType, followers.ToString(), following.ToString()};
-            return Settings;
 
+            List<string> Settings = new List<string>() { username, password, email, accountType, followers.ToString(), following.ToString() };
+            return Settings;
             // ver si efectivamente esta informacion proviene del usuario o si se adquiere de database.
 
+
         }
 
-        public List<string> infoUser()
+        public List<string> infoUser()                             //Entrega el username y el email del usuario
         {
-            return new List<string> { username,email };
+            return new List<string> { username, email };
         }
-        public string SearchedInfoUser()
+
+        public string SearchedInfoUser()                          //Entrega un string con el username y el email del usuario
         {
             return "Name: " + username + " " + "Email: " + email;
         }
 
-        public bool GetVerification()
+        public bool GetVerification()                            //Decide si tiene o no verificacion a partir de sus seguidores
         {
-            // Decide si tiene o no verificacion a partir de sus seguidores
+
             if (followers > 3)
             {
                 verified = true;
@@ -77,24 +94,13 @@ namespace FyBuzz_E2
                 return verified;
             }
         }
+        //--------------------------------------------------------------------------------------------------
 
-
-        // Diccionario de usuarios {key int, lista de palabras}
-        /*
-        public void AdminDeleteUser(DataBase data, int key)
+        //MÉTODOS QUE PUEDE UTILIZAR EL ADMIN
+        //--------------------------------------------------------------------------------------------------
+        public void AdminBanUser()                              // Cambiar el account tyoe a uno menor
         {
-            // if y fors para borrar la informacion del usuario
-            // acceder al diccionario con la clave key
-            if (key == registerNumber)
-            {
-                data.Load_Users().Remove(key);
-            }
 
-        }*/
-
-        public void AdminBanUser()
-        {
-            // Cambiar el account tyoe a uno menor
             if (accountType == "Premium")
             {
                 accountType.Replace("Premium", "Standard");
@@ -102,6 +108,23 @@ namespace FyBuzz_E2
 
         }
         // Encontrar mas metodos para admin +Admin...
+        //--------------------------------------------------------------------------------------------------
 
     }
 }
+
+
+//SE ENCONTRABA EN LA LINEA 82
+
+// Diccionario de usuarios {key int, lista de palabras}          
+/*
+public void AdminDeleteUser(DataBase data, int key)
+{
+    // if y fors para borrar la informacion del usuario
+    // acceder al diccionario con la clave key
+    if (key == registerNumber)
+    {
+        data.Load_Users().Remove(key);
+    }
+
+}*/
