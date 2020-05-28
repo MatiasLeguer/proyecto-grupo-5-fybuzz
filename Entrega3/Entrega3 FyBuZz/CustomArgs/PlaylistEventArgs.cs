e57a45a72990b7d310ab7acbf5f12dd17dcc207b
MatiasLeguer <62114519+MatiasLeguer@users.noreply.github.com>
2020-05-28 15:13:19 -0400

    ojala funcione

---
+++ Entrega3/Entrega3 FyBuZz/CustomArgs/PlaylistEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega3_FyBuZz.CustomArgs
+{
+    public class PlaylistEventArgs : EventArgs
+    {
+        public string NameText { get; set; }
+        public string FormatText { get; set; }
+        public string CreatorText { get; set; }
+        public string ProfileCreatorText { get; set; }
+    }
+}
