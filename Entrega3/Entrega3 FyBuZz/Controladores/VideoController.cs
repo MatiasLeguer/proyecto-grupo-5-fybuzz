27adf56fb7fa42a95fb2209129fd801d707e7ba8
MatiasLeguer <62114519+MatiasLeguer@users.noreply.github.com>
2020-05-30 18:46:21 -0400

    Agregado la clase VideoController y VideoEventArgs, al igual que un video de prueba

---
+++ Entrega3/Entrega3 FyBuZz/Controladores/VideoController.cs
@@ -0,0 +1,56 @@
+﻿using Entrega3_FyBuZz.CustomArgs;
+using Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entrega3_FyBuZz.Controladores
+{
+    public class VideoController
+    {
+        FyBuZz fyBuzz;
+        List<Video> videoDataBase = new List<Video>();
+        DataBase database = new DataBase();
+
+        public VideoController(Form fyBuzz)
+        {
+            Initialize();
+            this.fyBuzz = fyBuzz as FyBuZz;
+            this.fyBuzz.CreateVideoSaveButton_Clicked += OnCreateVideoSaveButton_Clicked;
+        }
+
+        public void Initialize()
+        {
+            if(File.Exists("AllVideos.bin") != true)
+            {
+                database.Save_Videos(videoDataBase);
+            }
+            else
+            {
+                videoDataBase = database.Load_Videos();
+            }
+        }
+
+        public bool OnCreateVideoSaveButton_Clicked(object sender, VideoEventArgs e)
+        {
+            File.Copy(e.FileDestText, e.FileNameText);
+            List<string> infoMult = new List<string>() {e.NameText, e.ActorsText, e.DirectorsText, e.ReleaseDateText, e.DimensionText, e.QualityText, e.Categorytext, e.DescriptionText, e.VideoImage, e.DurationText, e.SubtitlesText, e.FormatText, e.FileNameText};
+            string description = database.AddMult(1, infoMult, null, null, videoDataBase, null, null, null, null);
+            if(description == null)
+            {
+                database.Save_Videos(videoDataBase);
+                return true;
+            }
+            else
+            {
+                File.Delete(e.FileNameText);
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Entrega3/Entrega3 FyBuZz/CustomArgs/VideoEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega3_FyBuZz.CustomArgs
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public string NameText { get; set; }
+        public string ActorsText { get; set; }
+        public string DirectorsText { get; set; }
+        public string ReleaseDateText { get; set; }
+        public string DimensionText { get; set; }
+        public string QualityText { get; set; }
+        public string Categorytext { get; set; }
+        public string DescriptionText { get; set; }
+        public string VideoImage { get; set; }  /* Falta pasar bien la imagen */
+        public string DurationText { get; set; }
+        public string FormatText { get; set; }
+        public string SubtitlesText { get; set; }
+        public string FileDestText { get; set; }
+        public string FileNameText { get; set; }
+
+    }
+}
