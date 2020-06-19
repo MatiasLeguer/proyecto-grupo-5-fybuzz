8c530ecc304d4d9b844d6061ddef38a2d07915ff
VASQUEZ Jose <G511801@mph.morpho.com>
2020-06-19 15:13:55 -0400

    Arreglanzo cagazos estratosfericos a la wisin y yandeeeeel

---
+++ Interfaz_FyBuzz_MOD1/Interfaz_FyBuzz_MOD1/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaz_FyBuzz_MOD1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Diseno_Oculto();
+        }
+
+        private void Diseno_Oculto()
+        {
+            MultimediaIOptionsPanel.Visible = false;
+            PlayListsOptionsPanel.Visible = false;
+            CreateOptionsPanel.Visible = false;
+        }
+
+        private void OcultarSubMenus()
+        {
+            if(MultimediaIOptionsPanel.Visible == true)
+                MultimediaIOptionsPanel.Visible = false;
+            if (PlayListsOptionsPanel.Visible == true)
+                PlayListsOptionsPanel.Visible = false;
+            if (CreateOptionsPanel.Visible == true)
+                CreateOptionsPanel.Visible = false;
+        }
+
+        private void MostrarSubMenus(Panel SubMenu)
+        {
+            if (SubMenu.Visible == false)
+            {
+                OcultarSubMenus();
+                SubMenu.Visible = true;
+            }
+            else
+                SubMenu.Visible = false;
+        }
+
+        private void MultimediaButton_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenus(MultimediaIOptionsPanel);
+        }
+
+        private void PlayListsButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            MostrarSubMenus(PlayListsOptionsPanel);
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenus(CreateOptionsPanel);
+        }
+
+        private void SongsButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            OpenOverGeneralPanel(new Form2());
+            OcultarSubMenus();
+        }
+
+        private void VideosButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            OpenOverGeneralPanel(new Form2());
+            OcultarSubMenus();
+        }
+
+        private void PrivatePlsButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            OpenOverGeneralPanel(new Form3());
+            OcultarSubMenus();
+        }
+
+        private void GlobalPlsButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            OpenOverGeneralPanel(new Form3());
+            OcultarSubMenus();
+        }
+
+        private void FavoritePlsButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            OpenOverGeneralPanel(new Form3());
+            OcultarSubMenus();
+        }
+
+        private void CreateSongsButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            OpenOverGeneralPanel(new Form4());
+            OcultarSubMenus();
+        }
+
+        private void CreateVideosButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            OpenOverGeneralPanel(new Form4());
+            OcultarSubMenus();
+        }
+
+        private void CreatePlayListsButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            OpenOverGeneralPanel(new Form4());
+            OcultarSubMenus();
+        }
+
+        private void CreateProfilesButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            OpenOverGeneralPanel(new Form4());
+            OcultarSubMenus();
+        }
+
+        private void AboutUsButton_Click(object sender, EventArgs e)
+        {
+            //Codigo de Los Cabros....
+            
+        }
+
+        private void GeneralPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private Form DisplayActivo = null;
+        private void OpenOverGeneralPanel(Form DisplayForm)
+        {
+            if (DisplayActivo != null)
+                DisplayActivo.Close();
+            DisplayActivo = DisplayForm;
+            DisplayForm.TopLevel = false;
+            DisplayForm.FormBorderStyle = FormBorderStyle.None;
+            DisplayForm.Dock = DockStyle.Fill;
+            GeneralPanel.Controls.Add(DisplayForm);
+            GeneralPanel.Tag = DisplayForm;
+            DisplayForm.BringToFront();
+            DisplayForm.Show();
+
+        }
+    }
+}
---
+++ Interfaz_FyBuzz_MOD1/Interfaz_FyBuzz_MOD1/Form3.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Interfaz_FyBuzz_MOD1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GoBackButton = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // GoBackButton
+            // 
+            this.GoBackButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GoBackButton.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.GoBackButton.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GoBackButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.GoBackButton.IconChar = FontAwesome.Sharp.IconChar.Reply;
+            this.GoBackButton.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(188)))), ((int)(((byte)(45)))));
+            this.GoBackButton.IconSize = 40;
+            this.GoBackButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.GoBackButton.Location = new System.Drawing.Point(12, 12);
+            this.GoBackButton.Name = "GoBackButton";
+            this.GoBackButton.Rotation = 0D;
+            this.GoBackButton.Size = new System.Drawing.Size(124, 52);
+            this.GoBackButton.TabIndex = 1;
+            this.GoBackButton.Text = "Return";
+            this.GoBackButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.GoBackButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.GoBackButton.UseVisualStyleBackColor = true;
+            this.GoBackButton.Click += new System.EventHandler(this.GoBackButton_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(20)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(684, 438);
+            this.Controls.Add(this.GoBackButton);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconButton GoBackButton;
+    }
+}
